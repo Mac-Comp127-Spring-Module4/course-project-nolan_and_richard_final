e0859b5db8beea9477ed0bba95a83cd1f25b1e25
Nolan Meyer <78496256+nolan-meyer@users.noreply.github.com>
2021-04-29 11:54:44 -0500

    Added methods and outline to JumpPad

--- src/doodlejump/JumpPad.java
+++ src/doodlejump/JumpPad.java
@@ -1,5 +1,52 @@
 package doodlejump;
 
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Rectangle;
+
+
+/**
+ * 
+ */
 public class JumpPad {
+    private Rectangle jumpPadShape;
+    public static final double JUMP_PAD_LENGTH = 50;
+    public static final double JUMP_PAD_WIDTH = 10;
+    private double leftX, rightX, topY, bottomY;
+
+    public JumpPad(double leftX, double topY) {
+        this.leftX = leftX;
+        rightX = leftX + JUMP_PAD_LENGTH;
+        this.topY = topY;
+        bottomY = topY + JUMP_PAD_WIDTH;
+        
+        jumpPadShape = new Rectangle(leftX, topY, JUMP_PAD_LENGTH, JUMP_PAD_WIDTH);
+        
+        jumpPadShape.setFilled(true);
+    }
     
+
+    /**
+    * Adds the jump pad's shape to the given canvas.
+    * @param canvas
+    */
+    public void addToCanvas(CanvasWindow canvas) {
+        canvas.add(jumpPadShape);
+    }
+
+    /**
+    * Removes the jump pad's shape from the given canvas.
+    * @param canvas
+    */
+    public void removeFromCanvas(CanvasWindow canvas) {
+        canvas.remove(jumpPadShape);
+    }
+
+    /**
+     * Tests for intersections between the doodle and this jump pad.
+     * @return true If the doodle intersects the pad.
+     */
+    public boolean intersects(Doodle doodle, CanvasWindow canvas) {
+        //test for intersection
+        return true;
+    }
 }
