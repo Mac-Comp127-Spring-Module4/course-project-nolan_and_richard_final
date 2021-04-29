bca9187e326c92dfb591810edcb9c3099305f3eb
Nolan Meyer <78496256+nolan-meyer@users.noreply.github.com>
2021-04-29 10:43:25 -0500

    Adding method outlines to DoodleJumpGame

--- src/doodlejump/DoodleJumpGame.java
+++ src/doodlejump/DoodleJumpGame.java
@@ -1,5 +1,47 @@
 package doodlejump;
 
+import edu.macalester.graphics.CanvasWindow;
+
+
+/**
+ * 
+ */
 public class DoodleJumpGame {
-    
+   public static final int CANVAS_WIDTH = 800;
+   public static final int CANVAS_HEIGHT = 800;
+   
+   private final CanvasWindow canvas;
+
+
+   public DoodleJumpGame() {
+       canvas = new CanvasWindow("Doodle Jump Remastered", CANVAS_WIDTH, CANVAS_HEIGHT);
+   }
+
+
+    public static void main(String[] args) {
+       DoodleJumpGame game = new DoodleJumpGame();
+       // game.runGame();
+    }
+
+
+    /**
+     * 
+     */
+    public void setupGame() {
+
+    }
+
+    /**
+     * 
+     */
+    public void runGame() {
+
+    }
+
+    /**
+     * 
+     */
+    private void createDoodle() {
+
+    }
 }
