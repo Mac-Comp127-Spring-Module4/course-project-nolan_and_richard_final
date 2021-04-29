de995877779c9f9ecce023af4a387d199a852cb3
Nolan Meyer <78496256+nolan-meyer@users.noreply.github.com>
2021-04-29 12:28:11 -0500

    Removed excess variable in doodle

--- src/doodlejump/Doodle.java
+++ src/doodlejump/Doodle.java
@@ -10,16 +10,13 @@ public class Doodle {
     public static final double DOODLE_WIDTH = 25;
     public static final double GRAVITY = -9.8;
 
-    private double leftX, rightX, topY, bottomY;
+    private double leftX, topY;
     private double yVelo = -50;
 
 
     public Doodle(double leftX, double topY) {
         this.leftX = leftX;
-        rightX = leftX + DOODLE_WIDTH;
         this.topY = topY;
-        bottomY = topY + DOODLE_WIDTH;
-
 
         doodleShape = new Rectangle(leftX, topY, DOODLE_WIDTH, DOODLE_WIDTH);
 
@@ -41,24 +38,6 @@ public class Doodle {
     */
     public void removeFromCanvas(CanvasWindow canvas) {
         canvas.remove(doodleShape);
-    }
-
-    /**
-     * Update the doodle's position if it is in bounds
-     * @return true if the doodle is in within the window
-     */
-    public boolean updatePosition(double dt) {
-        double newY;
-        newY = topY + (yVelo * dt);
-        if(newY <= DoodleJumpGame.CANVAS_HEIGHT && newY >= 0){  //if doodle will still be in the window
-            topY = newY;
-            doodleShape.setPosition(leftX, topY);
-            yVelo -= (GRAVITY * dt);
-            return true;
-        }
-        else{
-            return false;
-        }
     }
 
     /**
@@ -84,6 +63,23 @@ public class Doodle {
         yVelo = -yVelo;
     }
 
+    /**
+     * Update the doodle's position if it is in bounds
+     * @return true if the doodle is in within the window
+     */
+    public boolean updatePosition(double dt) {
+        double newY;
+        newY = topY + (yVelo * dt);
+        if(newY <= DoodleJumpGame.CANVAS_HEIGHT && newY >= 0){  //if doodle will still be in the window
+            topY = newY;
+            doodleShape.setPosition(leftX, topY);
+            yVelo -= (GRAVITY * dt);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
     /**
      * Moves the doodle in accordance to the mouse location. Makes sure the doodle
@@ -94,22 +90,18 @@ public class Doodle {
         if (mouseCursor.getPosition().getX() > doodleShape.getX()){
             doodleShape.setPosition(mouseCursor.getPosition().getX(), topY);
             leftX = mouseCursor.getPosition().getX();
-            rightX = mouseCursor.getPosition().getX() + DOODLE_WIDTH;
         }
         if (mouseCursor.getPosition().getX() < doodleShape.getX()){
             doodleShape.setPosition(mouseCursor.getPosition().getX(), topY);
             leftX = mouseCursor.getPosition().getX();
-            rightX = mouseCursor.getPosition().getX() + DOODLE_WIDTH;
         }
         if (mouseCursor.getPosition().getX() <= 0){  //left-bound
             doodleShape.setPosition(0, topY);
             leftX = 0;
-            rightX = DOODLE_WIDTH;
         }
         if (mouseCursor.getPosition().getX() >= DoodleJumpGame.CANVAS_WIDTH - DOODLE_WIDTH){  //right-bound
             doodleShape.setPosition(DoodleJumpGame.CANVAS_WIDTH - DOODLE_WIDTH, topY);
             leftX = DoodleJumpGame.CANVAS_WIDTH - DOODLE_WIDTH;
-            rightX = DoodleJumpGame.CANVAS_WIDTH;
         }
     }
 }
